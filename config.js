6508d0a8ecf7dd4831cb7d2a01d17e4a9bd8250a
rdtyzbotz <113617871+rdtyzbotz@users.noreply.github.com>
2022-10-18 21:16:51 +0700

    Update config.js

--- config.js
+++ config.js
@@ -30,9 +30,9 @@ global.APIKeys = { // APIKey Here
   'https://leyscoders-api.herokuapp.com': 'dappakntlll'
   'https://api.lolhuman.xyz': 'Imanmon'
 }
-global.packname = data.namabot
-global.author = data.owner
-global.wm = data.namabot + ' ' + data.owner
+global.packname = 'gilzz azaa'
+global.author = 'gilzz azaa
+global.wm = 'Gilz-Botz'
 
 global.multiplier = 100 // The higher, The harder levelup
 
